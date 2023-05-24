c6869ef856c29bd5941f121ed6c70b2ab989275b
Vasiliy Isaichkin <vasiliy@isaichkin.ru>
2023-05-24 08:37:24 +0300

    Up deps

--- defs/classes/Serializer.d.ts
+++ defs/classes/Serializer.d.ts
@@ -5,12 +5,12 @@ interface SerializerPacketOptions {
     useCRC32: boolean;
     useSchema?: boolean;
 }
-export declare type SerializerSchema = Array<string>;
+export type SerializerSchema = Array<string>;
 export interface SerializerSchemaObject {
     id: number;
     fields: SerializerSchema;
 }
-declare type SerializerSchemaIdOrSchemaObject = SerializerSchemaObject | number | null;
+type SerializerSchemaIdOrSchemaObject = SerializerSchemaObject | number | null;
 export declare class Serializer {
     readonly version: number;
     private compressionLengthThreshold;
@@ -22,7 +22,9 @@ export declare class Serializer {
     use(id: number, detector: Function, encode: (arg: any) => any, decode: (arg: any) => any): void;
     registerSchema<T>(id: number, fields: T): void;
     unregisterSchema(id: number): void;
-    serialize<T extends Record<string, unknown>>(payload: T, schemaIdOrSchemaObject?: SerializerSchemaIdOrSchemaObject): Buffer;
+    serialize<T extends {
+        [key: string | number | symbol]: unknown;
+    }>(payload: T, schemaIdOrSchemaObject?: SerializerSchemaIdOrSchemaObject): Buffer;
     deserialize<T>(buf: Buffer | Uint8Array): T;
 }
 export {};
